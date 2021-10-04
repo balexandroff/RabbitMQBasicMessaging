27994fb6aa598cee0685c044ba7eab4cd495a80a
balexandrov <balexandrov@topviewnyc.com>
2021-10-04 13:09:21 +0300

    Add project files.

---
+++ Producer/Sender.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "127.0.0.1" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                    string message = ".NET Core RabbitMQ simple message.";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "BasicTest", null, body);
+                    Console.WriteLine($"Sent message {message}.");
+
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Receiver/Receiver.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "127.0.0.1" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine($"Received message {message}.");
+                    };
+
+                    channel.BasicConsume("BasicTest", true, consumer);
+
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
